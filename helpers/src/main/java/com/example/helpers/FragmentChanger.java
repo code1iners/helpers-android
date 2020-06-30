e88576054a3cd19aa679878261090c0046ce71c7
ethan <wy0353@gmail.com>
2020-06-30 19:07:47 +0900

    Add tools

--- helpers/src/main/java/com/example/helpers/FragmentChanger.java
+++ helpers/src/main/java/com/example/helpers/FragmentChanger.java
@@ -10,7 +10,7 @@ import androidx.fragment.app.FragmentTransaction;
 public class FragmentChanger {
     private static final String TAG = FragmentChanger.class.getSimpleName();
 
-    public static void replace(FragmentManager manager, int container, Fragment fragment, Bundle args, Boolean backStack) {
+    public static void replace(FragmentManager manager, int container, Fragment fragment, Boolean backStack, Bundle args) {
         Log.w(TAG, new Object(){}.getClass().getEnclosingMethod().getName());
         FragmentTransaction t = manager.beginTransaction();
 
@@ -18,11 +18,27 @@ public class FragmentChanger {
         else t.replace(container, fragment).commit();
     }
 
-    public static void add(FragmentManager manager, int container, Fragment fragment, Bundle args, Boolean backStack) {
+    public static void replaceWithAnim(FragmentManager manager, int container, Fragment fragment, Boolean backStack, Bundle args, int anim1, int anim2) {
+        Log.w(TAG, new Object(){}.getClass().getEnclosingMethod().getName());
+        FragmentTransaction t = manager.beginTransaction();
+
+        if (backStack) t.addToBackStack(null).setCustomAnimations(anim1, anim2).replace(container, fragment).commit();
+        else t.replace(container, fragment).commit();
+    }
+
+    public static void add(FragmentManager manager, int container, Fragment fragment, Boolean backStack, Bundle args) {
         Log.w(TAG, new Object(){}.getClass().getEnclosingMethod().getName());
         FragmentTransaction t = manager.beginTransaction();
 
         if (backStack) t.addToBackStack(null).add(container, fragment).commit();
+        else t.add(container, fragment).commit();
+    }
+
+    public static void addWithAnim(FragmentManager manager, int container, Fragment fragment, Boolean backStack, Bundle args, int anim1, int anim2) {
+        Log.w(TAG, new Object(){}.getClass().getEnclosingMethod().getName());
+        FragmentTransaction t = manager.beginTransaction();
+
+        if (backStack) t.addToBackStack(null).setCustomAnimations(anim1, anim2).add(container, fragment).commit();
         else t.add(container, fragment).commit();
     }
 }
