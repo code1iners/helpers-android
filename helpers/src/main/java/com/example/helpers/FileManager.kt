a67559569ae60dedcd596450c376715291ea51c2
TakeMyHands <ethan92subsub@gmail.com>
2020-08-13 12:47:26 +0900

    Add remove directory method

--- helpers/src/main/java/com/example/helpers/FileManager.kt
+++ helpers/src/main/java/com/example/helpers/FileManager.kt
@@ -11,81 +11,102 @@ import java.io.File
 import java.lang.Exception
 
 class FileManager {
-    fun removeFile(filePath: String?) {
-        Log.w(TAG, object : Any(){}.javaClass.enclosingMethod!!.name)
-        Log.i(TAG, "filePath : $filePath")
-
-        val file = File(filePath)
-        Log.v(TAG, "file : $file")
-        if (file.exists()) {
-            if (file.delete()) Log.i(TAG, "File delete success")
-            else Log.i(TAG, "File delete fail")
-        }
-    }
-
-    fun createDir(dirPath: String?) {
-        Log.w(TAG, object : Any(){}.javaClass.enclosingMethod!!.name)
-        val dir = File(dirPath)
-        if (!dir.exists()) {
-            dir.mkdirs()
-            Log.e(TAG, "Created directory : $dir")
-        }
-    }
-
-    fun getFileNameFromUri(context: Context, uri: Uri): String {
-        Log.w(TAG, object : Any(){}.javaClass.enclosingMethod!!.name)
-        var result: String? = null
-        if (uri.scheme.equals("content")) {
-            val cursor: Cursor? = context.contentResolver.query(uri, null, null, null, null)
-            try {
-                if (cursor != null && cursor.moveToFirst()) {
-                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME))
-                }
-            } finally {
-                cursor!!.close()
-            }
-        }
-        if (result == null) {
-            result = uri.path
-            val cut = result?.lastIndexOf('/')
-            if (cut != -1) {
-                result = result?.substring(cut!! + 1)
-            }
-        }
-        result = result!!.replace(" ", "_")
-
-        return result
-    }
-
-    fun getVideoPathByUri(activity: Activity, uri: Uri): String? {
-        Log.w(TAG, object:Any(){}.javaClass.enclosingMethod!!.name)
-        var result = ""
-        try {
-            val proj = arrayOf(MediaStore.Video.Media.DATA)
-            val cursor = activity.contentResolver.query(uri, proj, null, null, null)
-            activity.startManagingCursor(cursor)
-            val columnIndex = cursor?.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
-            cursor?.moveToFirst()
-            result = cursor?.getString(columnIndex!!)!!
-        } catch (e: Exception) {e.printStackTrace()}
-        return result
-    }
-
-    fun getImagePathByUri(activity: Activity, uri: Uri): String? {
-        Log.w(TAG, object:Any(){}.javaClass.enclosingMethod!!.name)
-        var result = ""
-        try {
-            val proj = arrayOf(MediaStore.Images.Media.DATA)
-            val cursor = activity.contentResolver.query(uri, proj, null, null, null)
-            activity.startManagingCursor(cursor)
-            val columnIndex = cursor?.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
-            cursor?.moveToFirst()
-            result = cursor?.getString(columnIndex!!)!!
-        } catch (e: Exception) {e.printStackTrace()}
-        return result
-    }
-
     companion object {
         private val TAG = FileManager::class.java.simpleName
+
+        fun removeFile(filePath: String?) {
+            Log.w(TAG, object : Any(){}.javaClass.enclosingMethod!!.name)
+            Log.i(TAG, "filePath : $filePath")
+
+            val file = File(filePath)
+            Log.v(TAG, "file : $file")
+            if (file.exists()) {
+                if (file.delete()) Log.i(TAG, "File delete success")
+                else Log.i(TAG, "File delete fail")
+            }
+        }
+
+        fun createDir(dirPath: String?) {
+            Log.w(TAG, object : Any(){}.javaClass.enclosingMethod!!.name)
+            try {
+                val dir = File(dirPath)
+                if (!dir.exists()) {
+                    dir.mkdirs()
+                    Log.e(TAG, "Created directory : $dir")
+                }
+            } catch (e: Exception) {e.printStackTrace()}
+        }
+
+        fun removeDir(dirPath: String?) {
+            Log.w(TAG, object : Any(){}.javaClass.enclosingMethod!!.name)
+            try {
+                // note. null check
+                if (dirPath.isNullOrEmpty()) return
+                // note. set directory
+                val dir = File(dirPath)
+                if (dir.exists()) {
+                    val childFiles = dir.listFiles()
+                    for (file in childFiles) {
+                        if (file.isDirectory) { removeDir(file.absolutePath) }
+                        else file.delete()
+                    }
+                    // note. delete dir
+                    dir.delete()
+                }
+            } catch (e: Exception) {e.printStackTrace()}
+        }
+
+        fun getFileNameFromUri(context: Context, uri: Uri): String {
+            Log.w(TAG, object : Any(){}.javaClass.enclosingMethod!!.name)
+            var result: String? = null
+            if (uri.scheme.equals("content")) {
+                val cursor: Cursor? = context.contentResolver.query(uri, null, null, null, null)
+                try {
+                    if (cursor != null && cursor.moveToFirst()) {
+                        result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME))
+                    }
+                } finally {
+                    cursor!!.close()
+                }
+            }
+            if (result == null) {
+                result = uri.path
+                val cut = result?.lastIndexOf('/')
+                if (cut != -1) {
+                    result = result?.substring(cut!! + 1)
+                }
+            }
+            result = result!!.replace(" ", "_")
+
+            return result
+        }
+
+        fun getVideoPathByUri(activity: Activity, uri: Uri): String? {
+            Log.w(TAG, object:Any(){}.javaClass.enclosingMethod!!.name)
+            var result = ""
+            try {
+                val proj = arrayOf(MediaStore.Video.Media.DATA)
+                val cursor = activity.contentResolver.query(uri, proj, null, null, null)
+                activity.startManagingCursor(cursor)
+                val columnIndex = cursor?.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+                cursor?.moveToFirst()
+                result = cursor?.getString(columnIndex!!)!!
+            } catch (e: Exception) {e.printStackTrace()}
+            return result
+        }
+
+        fun getImagePathByUri(activity: Activity, uri: Uri): String? {
+            Log.w(TAG, object:Any(){}.javaClass.enclosingMethod!!.name)
+            var result = ""
+            try {
+                val proj = arrayOf(MediaStore.Images.Media.DATA)
+                val cursor = activity.contentResolver.query(uri, proj, null, null, null)
+                activity.startManagingCursor(cursor)
+                val columnIndex = cursor?.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+                cursor?.moveToFirst()
+                result = cursor?.getString(columnIndex!!)!!
+            } catch (e: Exception) {e.printStackTrace()}
+            return result
+        }
     }
 }
