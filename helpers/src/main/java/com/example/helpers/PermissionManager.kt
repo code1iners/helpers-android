5cbcd44f50c3e2232bb833699470fee85c6b8a98
TakeMyHands <ethan92subsub@gmail.com>
2020-08-15 15:24:28 +0900

    Update permission manager related to denied

--- helpers/src/main/java/com/example/helpers/PermissionManager.kt
+++ helpers/src/main/java/com/example/helpers/PermissionManager.kt
@@ -10,6 +10,7 @@ import android.util.Log
 import android.view.View
 import com.google.android.material.snackbar.Snackbar
 
+
 class PermissionManager {
     object RequestCode {
         const val WRITE_EXTERNAL_STORAGE = 100
@@ -52,39 +53,36 @@ class PermissionManager {
             Log.w(TAG, object:Any(){}.javaClass.enclosingMethod!!.name)
             try {
                 Log.e(TAG, "##1:${activity.shouldShowRequestPermissionRationale(permission)}")
-                if (activity.shouldShowRequestPermissionRationale(permission)) {
-                    val snack = Snackbar.make(view, "해당 기능을 사용하기 위해서는 권한이 필요합니다.", Snackbar.LENGTH_INDEFINITE)
-                    snack.setAction("설정") {
-                        when (permission) {
-                            android.Manifest.permission.WRITE_EXTERNAL_STORAGE -> {activity.requestPermissions(arrayOf(permission), RequestCode.WRITE_EXTERNAL_STORAGE)}
-                            android.Manifest.permission.INTERNET -> {activity.requestPermissions(arrayOf(permission), RequestCode.INTERNET)}
-                            android.Manifest.permission.CAMERA -> {activity.requestPermissions(arrayOf(permission), RequestCode.CAMERA)}
-                            android.Manifest.permission.READ_EXTERNAL_STORAGE -> {activity.requestPermissions(arrayOf(permission), RequestCode.READ_EXTERNAL_STORAGE)}
-                            android.Manifest.permission.RECORD_AUDIO -> {activity.requestPermissions(arrayOf(permission), RequestCode.RECORD_AUDIO)}
-                            android.Manifest.permission.BROADCAST_STICKY -> {activity.requestPermissions(arrayOf(permission), RequestCode.BROADCAST_STICKY)}
-                            android.Manifest.permission.MODIFY_AUDIO_SETTINGS -> {activity.requestPermissions(arrayOf(permission), RequestCode.MODIFY_AUDIO_SETTINGS)}
-                            android.Manifest.permission.BLUETOOTH -> {activity.requestPermissions(arrayOf(permission), RequestCode.BLUETOOTH)}
-                            android.Manifest.permission.BLUETOOTH_ADMIN -> {activity.requestPermissions(arrayOf(permission), RequestCode.BLUETOOTH_ADMIN)}
-                            android.Manifest.permission.ACCESS_COARSE_LOCATION -> {activity.requestPermissions(arrayOf(permission), RequestCode.ACCESS_COARSE_LOCATION)}
-                            android.Manifest.permission.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS -> {activity.requestPermissions(arrayOf(permission), RequestCode.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS)}
-                            android.Manifest.permission.RECEIVE_BOOT_COMPLETED -> {activity.requestPermissions(arrayOf(permission), RequestCode.RECEIVE_BOOT_COMPLETED)}
-                            android.Manifest.permission.FOREGROUND_SERVICE -> {activity.requestPermissions(arrayOf(permission), RequestCode.FOREGROUND_SERVICE)}
-                            android.Manifest.permission.WAKE_LOCK -> {activity.requestPermissions(arrayOf(permission), RequestCode.WAKE_LOCK)}
-                            android.Manifest.permission.ACCESS_NETWORK_STATE -> {activity.requestPermissions(arrayOf(permission), RequestCode.ACCESS_NETWORK_STATE)}
-                        }
-                        snack.dismiss()
+                val snack = Snackbar.make(view, "해당 기능을 사용하기 위해서는 권한이 필요합니다.", Snackbar.LENGTH_INDEFINITE)
+                snack.setAction("설정") {
+                    when (permission) {
+                        android.Manifest.permission.WRITE_EXTERNAL_STORAGE -> {activity.requestPermissions(arrayOf(permission), RequestCode.WRITE_EXTERNAL_STORAGE)}
+                        android.Manifest.permission.INTERNET -> {activity.requestPermissions(arrayOf(permission), RequestCode.INTERNET)}
+                        android.Manifest.permission.CAMERA -> {activity.requestPermissions(arrayOf(permission), RequestCode.CAMERA)}
+                        android.Manifest.permission.READ_EXTERNAL_STORAGE -> {activity.requestPermissions(arrayOf(permission), RequestCode.READ_EXTERNAL_STORAGE)}
+                        android.Manifest.permission.RECORD_AUDIO -> {activity.requestPermissions(arrayOf(permission), RequestCode.RECORD_AUDIO)}
+                        android.Manifest.permission.BROADCAST_STICKY -> {activity.requestPermissions(arrayOf(permission), RequestCode.BROADCAST_STICKY)}
+                        android.Manifest.permission.MODIFY_AUDIO_SETTINGS -> {activity.requestPermissions(arrayOf(permission), RequestCode.MODIFY_AUDIO_SETTINGS)}
+                        android.Manifest.permission.BLUETOOTH -> {activity.requestPermissions(arrayOf(permission), RequestCode.BLUETOOTH)}
+                        android.Manifest.permission.BLUETOOTH_ADMIN -> {activity.requestPermissions(arrayOf(permission), RequestCode.BLUETOOTH_ADMIN)}
+                        android.Manifest.permission.ACCESS_COARSE_LOCATION -> {activity.requestPermissions(arrayOf(permission), RequestCode.ACCESS_COARSE_LOCATION)}
+                        android.Manifest.permission.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS -> {activity.requestPermissions(arrayOf(permission), RequestCode.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS)}
+                        android.Manifest.permission.RECEIVE_BOOT_COMPLETED -> {activity.requestPermissions(arrayOf(permission), RequestCode.RECEIVE_BOOT_COMPLETED)}
+                        android.Manifest.permission.FOREGROUND_SERVICE -> {activity.requestPermissions(arrayOf(permission), RequestCode.FOREGROUND_SERVICE)}
+                        android.Manifest.permission.WAKE_LOCK -> {activity.requestPermissions(arrayOf(permission), RequestCode.WAKE_LOCK)}
+                        android.Manifest.permission.ACCESS_NETWORK_STATE -> {activity.requestPermissions(arrayOf(permission), RequestCode.ACCESS_NETWORK_STATE)}
                     }
-                    snack.show()
-                } else {
-                    Snackbar.make(view, "권한에 대한 승인이 거부되었습니다.\n앱 정보에서 해당 권한을 직접 설정해주세요.", Snackbar.LENGTH_LONG).show()
-                    showDialogPermission(activity, permission)
+                    snack.dismiss()
                 }
+                snack.show()
             } catch (e: Exception) {e.printStackTrace()}
         }
 
-        fun showDialogPermission(activity: Activity, permission: String) {
+        fun showDialogPermission(activity: Activity, view: View, permission: String) {
             Log.w(TAG, object:Any(){}.javaClass.enclosingMethod!!.name)
             try {
+                Snackbar.make(view, "권한에 대한 승인이 거부되었습니다.\n앱 정보에서 해당 권한을 직접 설정해주세요.", Snackbar.LENGTH_LONG).show()
+
                 val builder = AlertDialog.Builder(activity)
                 builder.setTitle("권한 요청")
 
