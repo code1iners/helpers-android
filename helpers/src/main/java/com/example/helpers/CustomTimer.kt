6de84fd91ef847ff68328bce0e245917a7f1417f
TakeMyHands <ethan92subsub@gmail.com>
2020-09-18 22:29:49 +0900

    Add current timer in Custom timer

--- helpers/src/main/java/com/example/helpers/CustomTimer.kt
+++ helpers/src/main/java/com/example/helpers/CustomTimer.kt
@@ -287,4 +287,38 @@ class CustomTimer {
             val TAG = Decrease::class.simpleName
         }
     }
+
+    // note. current time
+    class CurrentTimer: TimerTask() {
+
+        // note. vars-boolean
+        var isStarted = false
+        var isPause = false
+        // note. vars-listener
+        lateinit var currentTimerListener: CurrentTimerListener
+
+        fun start() {
+            Timer().schedule(this, 0,1000)
+            isStarted = true
+        }
+
+        override fun run() {
+            if (isPause) return
+
+            val date = Calendar.getInstance().time
+            val year = date.year
+            val hours = date.hours
+            val minutes = date.minutes
+
+            currentTimerListener.print(year = year, hours = hours, minutes = minutes)
+        }
+
+        companion object {
+            val TAG = Timer::class.simpleName
+        }
+
+        interface CurrentTimerListener {
+            fun print(year: Int, hours: Int, minutes: Int)
+        }
+    }
 }
