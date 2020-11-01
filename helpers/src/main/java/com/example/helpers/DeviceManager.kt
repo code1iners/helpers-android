491bdd7aedbebb888164e7f55a1efa0de9b36a46
TakeMyHands <ethan92subsub@gmail.com>
2020-11-01 11:08:36 +0900

    updated device manager, deleted android manager 2

--- helpers/src/main/java/com/example/helpers/DeviceManager.kt
+++ helpers/src/main/java/com/example/helpers/DeviceManager.kt
@@ -1,5 +1,8 @@
 package com.example.helpers
 
+import android.content.Context
+import android.os.Build
+import android.provider.Settings
 import android.util.Log
 
 class DeviceManager {
@@ -9,6 +12,23 @@ class DeviceManager {
         fun getDeviceVersion(): Int {
             Log.w(TAG, object:Any(){}.javaClass.enclosingMethod!!.name)
             return android.os.Build.VERSION.SDK_INT
+        }
+        
+        fun getDeviceId(context: Context): String {
+            return Settings.Secure.getString(context.contentResolver, Settings.Secure.ANDROID_ID)
+        }
+    
+        fun getDeviceModel(): String {
+            return Build.MODEL
+        }
+    
+        fun getDeviceOs(): String {
+            return Build.VERSION.RELEASE.toString()
+        }
+    
+        fun getAppVersion(context: Context): String {
+            val info = context.packageManager.getPackageInfo(context.packageName, 0)
+            return info.versionName
         }
     }
 }
