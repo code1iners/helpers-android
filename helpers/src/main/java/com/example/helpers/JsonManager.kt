fe0a300f20ad5e21e20af39a6335bfb79a34a838
ethan <wy0353@gmail.com>
2020-11-26 10:22:36 +0900

    updated json manager

--- helpers/src/main/java/com/example/helpers/JsonManager.kt
+++ helpers/src/main/java/com/example/helpers/JsonManager.kt
@@ -8,8 +8,9 @@ import org.json.JSONObject
 
 object JsonManager {
     val TAG = JsonManager::class.simpleName
-
-    fun jsonToJson(parameter: JSONObject): JsonObject {
+    
+    fun jsonToJson(parameter: JSONObject?): JsonObject? {
+        if (parameter == null) return null
         return JsonParser().parse(parameter.toString()) as JsonObject
     }
     
@@ -17,7 +18,7 @@ object JsonManager {
         return JsonParser().parse(parameter) as JsonObject
     }
 
-    fun resultOnlyOk(): JsonObject {
+    fun resultOnlyOk(): JsonObject? {
         return jsonToJson(JSONObject().apply {
             this.put("status", HTTP_200_OK)
         })
